fa44e56b4a4d693469e5349f09238df94d696135
Franc <francbruzja@gmail.com>
2020-04-24 11:51:01 +0200

    add draggable and droppable prototype

--- App.js
+++ App.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, View, Text, Animated } from "react-native";
+
+import { createDndContext } from "react-native-easy-dnd";
+
+const { Provider, Droppable, Draggable } = createDndContext();
+
+const img =
+  "https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Provider>
+        <View>
+          <Draggable
+            onDragStart={() => {
+              console.log("Started draggging");
+            }}
+            onDragEnd={() => {
+              console.log("Ended draggging");
+            }}
+            payload="my-draggable-item"
+          >
+            {({ viewProps }) => {
+              return (
+                <Animated.View
+                  {...viewProps}
+                  style={[
+                    viewProps.style,
+                    { width: 200, height: 200, backgroundColor: "red" },
+                  ]}
+                >
+                  <Text style={{ color: "#fff", fontWeight: "bold" }}>
+                    Drag me
+                  </Text>
+                </Animated.View>
+              );
+            }}
+          </Draggable>
+          <Droppable
+            onEnter={() => {
+              console.log("Draggable entered");
+            }}
+            onLeave={() => {
+              console.log("Draggable left");
+            }}
+            onDrop={({ payload }) => {
+              console.log(
+                "Draggable with the following payload was dropped",
+                payload
+              );
+            }}
+          >
+            {({ active, viewProps }) => {
+              return (
+                <Animated.View
+                  {...viewProps}
+                  style={[
+                    {
+                      width: 300,
+                      height: 200,
+                      backgroundColor: active ? "blue" : "green",
+                    },
+                    viewProps.style,
+                  ]}
+                >
+                  <Text style={{ fontWeight: "bold", color: "white" }}>
+                    Drop here
+                  </Text>
+                </Animated.View>
+              );
+            }}
+          </Droppable>
+        </View>
+      </Provider>
     </View>
   );
 }
@@ -12,8 +81,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
